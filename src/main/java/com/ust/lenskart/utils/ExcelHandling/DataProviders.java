354d7537f78c852fd03f4662497c9cdf7ae6312e
Akshay A S(UST <268850@ust.com>
2024-04-30 09:49:01 +0530

    Adds excel handling classes

---
+++ src/main/java/com/ust/lenskart/utils/ExcelHandling/DataProviders.java
@@ -0,0 +1,24 @@
+package com.ust.lenskart.utils.ExcelHandling;
+
+
+import org.testng.annotations.DataProvider;
+
+import com.ust.lenskart.base.TestBase;
+
+public class DataProviders extends TestBase {
+	@DataProvider(name = "chatBotSuggestion")
+	public String[][] getData() {
+		String path = System.getProperty("user.dir")+"\\src\\main\\java\\testdata\\ChatBot Chat Suggestions.xlsx";
+		String Sheetname = testconfig.getSheetName();
+		XLutility xl = new XLutility(path,Sheetname);
+		int rownum=xl.getRowCount();
+		int colnum=xl.getCellCount(1);
+		String[][] arr=new String[rownum][colnum];
+		for (int i=1;i<=rownum;i++) {
+			for (int j=0;j<colnum;j++) {
+				arr[i-1][j]=xl.getCellData(i, j);
+			}
+		}
+		return arr;
+	}
+}
